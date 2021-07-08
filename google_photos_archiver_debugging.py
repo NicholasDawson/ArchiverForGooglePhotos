63558aeba90fa601f28a59d75c4c710ed41c7f5c
Nicholas Dawson <37987430+NicholasDawson@users.noreply.github.com>
2021-07-07 22:45:19 -0400

    Update google_photos_archiver_debugging.py
    Former-commit-id: 5491b2538e437b56136c62f4bd9072ba8aa5d84c
    Former-commit-id: 4ab0f43cd2c43fd453831c59ba6029e3c76742b1

--- google_photos_archiver_debugging.py
+++ google_photos_archiver_debugging.py
@@ -74,7 +74,7 @@ next = results['nextPageToken']
 
 while True:
     results = service.mediaItems().list(pageSize=100, pageToken=next).execute()
-    with open('result' + media_num + '.json', 'w') as f:
+    with open('result' + str(media_num) + '.json', 'w') as f:
         print('saving results...')
         f.write(json.dumps(results))
     media_num = download_images(results['mediaItems'], media_num)
